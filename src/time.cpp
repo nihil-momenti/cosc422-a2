e28f50d71557f76aee155ad8446bce80133fe6c0
Wim Looman <ghostunderscore@gmail.com>
2011-09-16 21:40:24 +1200

    Added high frequency timer for windows

--- src/time.cpp
+++ src/time.cpp
@@ -2,8 +2,21 @@
 
 #ifdef WIN32
 
+BOOL time_high_res;
+LARGE_INTEGER time_freq;
+LARGE_INTEGER time_count;
+
 double time_get() {
-    return GetTickCount();
+    if (time_high_res) {
+        QueryPerformanceCount(&time_count);
+        return (time_count * 1.0) / time_freq * 1000000.0;
+    } else {
+        return GetTickCount() * 1000.0;
+    }
+}
+
+void time_init() {
+    high_res = QueryPerformanceFrequency(&freq);
 }
 
 #else
@@ -12,7 +25,10 @@ double time_get() {
 double time_get() {
     timeval time;
     gettimeofday(&time, NULL);
-    return time.tv_sec * 1000.0 + time.tv_usec / 1000.0;
+    return time.tv_sec * 1000000.0 + time.tv_usec;
+}
+
+void time_init() {
 }
 
 #endif
--- src/time.hpp
+++ src/time.hpp
@@ -1,6 +1,9 @@
 #ifndef TIME_HPP
 #define TIME_HPP
 
+extern void time_init();
+
+// Returns the current time in microseconds
 extern double time_get();
 
 #endif
