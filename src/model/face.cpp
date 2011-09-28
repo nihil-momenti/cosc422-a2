3d8c4a950c7fc044011da7fc30a516e03d40d5ed
Wim Looman <ghostunderscore@gmail.com>
2011-09-28 23:54:00 +1300

    Added start of multi-texturing in faces

--- src/model/face.cpp
+++ src/model/face.cpp
@@ -34,18 +34,48 @@ double Face::area() {
     return mem_area;
 }
 
+//void Face::display() {
+//    Edge *e = edge;
+//
+//    do {
+//        Point  point =  e->vert->point;
+//        Vector normal = e->vert->normal();
+//        Vector dir = e->vert->point - edge->vert->point;
+//
+//        glMultiTexCoord2d(GL_TEXTURE0, 0.5, 0.5);
+//        glMultiTexCoord2d(GL_TEXTURE1, 0.5, 0.5);
+//        glMultiTexCoord2d(GL_TEXTURE2, 0.5, 0.5);
+//
+//        glNormal3d(normal.dx, normal.dy, normal.dz);
+//        glVertex3d(point.x, point.y, point.z);
+//
+//        e = e->next;
+//    } while (e != edge);
+//}
+
 void Face::display() {
-    Edge *e = edge;
+    Vert *v1 = edge->vert,
+         *v2 = edge->next->vert,
+         *v3 = edge->prev->vert;
 
-    do {
-        Point  point =  e->vert->point;
-        Vector normal = e->vert->normal();
-        Vector dir = e->vert->point - edge->vert->point;
+    glMultiTexCoord2d(GL_TEXTURE0, 0.5, 0.5);
+    glMultiTexCoord2d(GL_TEXTURE1, 0.5, 0.5);
+    glMultiTexCoord2d(GL_TEXTURE2, 0.5, 0.5);
 
-        glNormal3d(normal.dx, normal.dy, normal.dz);
-        glTexCoord3d(0.5+10*dir.dx, 0.5+10*dir.dy, 0.0);
-        glVertex3d(point.x, point.y, point.z);
+    glNormal3d(v1->normal().dx, v1->normal().dy, v1->normal().dz);
+    glVertex3d(v1->point.x, v1->point.y, v1->point.z);
 
-        e = e->next;
-    } while (e != edge);
+    glMultiTexCoord2d(GL_TEXTURE0, 0.5 + 0.5 * (v2->point - v1->point).unit().dx, 0.5 + 0.5 * (v2->point - v1->point).unit().dy);
+    glMultiTexCoord2d(GL_TEXTURE1, 0.5 + 0.5 * (v2->point - v1->point).unit().dx, 0.5 + 0.5 * (v2->point - v1->point).unit().dy);
+    glMultiTexCoord2d(GL_TEXTURE2, 0.5 + 0.5 * (v2->point - v1->point).unit().dx, 0.5 + 0.5 * (v2->point - v1->point).unit().dy);
+
+    glNormal3d(v2->normal().dx, v2->normal().dy, v2->normal().dz);
+    glVertex3d(v2->point.x, v2->point.y, v2->point.z);
+
+    glMultiTexCoord2d(GL_TEXTURE0, 0.5 + 0.5 * (v3->point - v1->point).unit().dx, 0.5 + 0.5 * (v3->point - v1->point).unit().dy);
+    glMultiTexCoord2d(GL_TEXTURE1, 0.5 + 0.5 * (v3->point - v1->point).unit().dx, 0.5 + 0.5 * (v3->point - v1->point).unit().dy);
+    glMultiTexCoord2d(GL_TEXTURE2, 0.5 + 0.5 * (v3->point - v1->point).unit().dx, 0.5 + 0.5 * (v3->point - v1->point).unit().dy);
+
+    glNormal3d(v3->normal().dx, v3->normal().dy, v3->normal().dz);
+    glVertex3d(v3->point.x, v3->point.y, v3->point.z);
 }
